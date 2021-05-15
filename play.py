fc01f50f545e290dcbd8f8e67bbec0f624f35df8
Yvonne Yeh <code@yvonneyeh.com>
2021-05-14 17:59:58 -0700

    Add .gitignore

--- play.py
+++ play.py
@@ -1,1 +1,14 @@
 from ufo import ufo
+
+noun_file = open("nouns.txt", "r")
+content = noun_file.read()
+noun_list = content.split("\n")
+noun_file.close()
+
+msg_file = open("messages.txt", "r")
+content = msg_file.read()
+msg_list = content.split("\n")
+msg_file.close()
+
+print(noun_list)
+# print(msg_list)
