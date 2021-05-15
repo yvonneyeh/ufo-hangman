dc2c6ece6cfdcf5730c45e12f94145883a476708
Yvonne Yeh <code@yvonneyeh.com>
2021-05-14 20:30:03 -0700

    Create game variables and set-up

--- play.py
+++ play.py
@@ -1,4 +1,5 @@
-import random 
+import random
+import string 
 from ufo import ufo
 
 # NOUN LIST
@@ -25,10 +26,39 @@ def get_word():
         word = random.choice(noun_list)
     # print(word.upper())
     return word.upper()
-    
+
+
+def show_ufo(attempts):
+    """ Return current UFO for attempt number."""
+
+    return ufo[attempts]
+
+
+def play():
+    word = get_word()
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    codeword = "_ " * len(word)
+    guessed = False
+    guessed_letters = set()
+    attempts = 0
+    incorrect = None
+
+    print("UFO: The Game")
+    print("Instructions: save us from alien abduction by guessing letters in the codeword.")
+    print("\n")
+    print(show_ufo(attempts))
+    print("\n")
+    print("Incorrect Guesses:")
+    print(incorrect)
+    print("\n")
+    print("Codeword:")
+    print(codeword)
+    print("\n")
+
+
 def play_ufo():
     pass
 
 if __name__ == '__main__':
-    get_word()
-    # play_ufo()
+    play()
