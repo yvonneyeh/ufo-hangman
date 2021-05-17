bcbd226705da1695e15aec7450a8f15ace9fac30
Yvonne Yeh <code@yvonneyeh.com>
2021-05-16 21:25:46 -0700

    Implement unittest.mock - in progress

--- tests.py
+++ tests.py
@@ -6,17 +6,29 @@ from unittest.mock import patch
 
 class TestGame(unittest.TestCase):
 
-    def test_correct_letter(self):
+
+    @patch('play.play(word)', return_value='o')
+    def test_correct_letter(self, input):
         """
         Test for when user guesses a correct letter.
         """
-        self.assertIn("Correct! You're closer to cracking the codeword.")
+        self.assertIn(play.play('word'), "Correct! You're closer to cracking the codeword.")
 
-    def test_incorrect_letter(self):
+
+    @patch('play.play(word)', return_value='a')
+    def test_incorrect_letter(self, input):
         """
         Test for when user guesses an incorrect letter.
         """
-        self.assertIn("Incorrect! The tractor beam pulls the person in further.")
+        self.assertIn(play.play('word'), "Incorrect! The tractor beam pulls the person in further.")
+
+
+    @patch('play.play_game()', return_value='N')
+    def test_no_play_again(self, input):
+        """
+        Test for when user does not want to continue game.
+        """
+        self.assertEqual(play.play_game(), 'Goodbye!')
 
 
 if __name__ == '__main__':
